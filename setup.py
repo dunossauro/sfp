55ce5fd17a630d8461737cceb12e08b306c73ee5
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2018-03-15 13:51:05 -0300

    Tox structures :heart:

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sfp',
+      version='0.0.8',
+      description='Simple Functional programming',
+      url='https://github.com/z4r4tu5tr4/sfp',
+      author='Eduardo Mendes',
+      author_email='mendesxeduardo@gmail.com',
+      license='MIT',
+      packages=['sfp'],
+      zip_safe=False)
