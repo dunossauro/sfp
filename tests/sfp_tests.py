401b0f9c62b98ebd90494c323ec0371a99c77f3c
Cyrax <cyraxtsung2@gmail.com>
2018-03-16 19:18:02 -0300

    Update sfp_tests.py

--- tests/sfp_tests.py
+++ tests/sfp_tests.py
@@ -1,21 +1,36 @@
 from unittest import TestCase, main
-import sys
-sys.path.append('../')
-
-from sfp import *
+from sfp import tail, pipe
 
 
-class tests(TestCase):
+class testTail(TestCase):
+    def test_should_get_tail_to_list(self):
+        self.assertEqual(tail([1, 2, 3, 4]), [2, 3, 4])
 
-    def test_pipe(self):
+    def test_should_get_tail_to_iterable(self):
+        result = tail(iter([1, 2, 3, 4]))
+        self.assertEqual(result, [2, 3, 4])
+
+
+class testsPipe(TestCase):
+    def test_pipe_should_return_a_callable(self):
+        """Check if pipe is a closure."""
+        _callable = pipe(lambda x: x)
+
+        self.assertTrue(hasattr(_callable, '__call__'))
+
+    def test_pipe_should_return_the_same_value_using_indentity_function(self):
+        self.assertEqual(pipe(lambda x: x)(5), 5)
+
+    def test_pipe_sequence_funtions_executions(self):
+        """This test fail because it is not a pipe, is a compose."""
         resul = pipe(
-            lambda x: x ** 2,
-            lambda x: x * 5,
-            lambda x: x // 5
-        )(5)
+            str.split,
+            len,
+            lambda x: x + 5
+        )('eduardo mendes')
 
-        self.assertEqual(resul, 25)
+        self.assertEqual(resul, 7)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
