20c039150a37c67320e9837f555f29d17db93ef0
Kaio Duarte <duartedossantos@gmail.com>
2018-03-16 19:08:44 -0300

    Add head function and tests

--- sfp/sfp.py
+++ sfp/sfp.py
@@ -5,3 +5,8 @@ def tail(iterable):
     """Get tail of a iterable is everything except the first element."""
     r = [x for x in iterable]
     return r[1:]
+
+
+def head(iterable: iter, n: int=1) -> iter:
+    """Get head of an iterable is the upper part of iterable."""
+    return iterable[:n]
--- tests/sfp_tests.py
+++ tests/sfp_tests.py
@@ -1,5 +1,5 @@
 from unittest import TestCase, main
-from sfp import tail
+from sfp import tail, head
 
 
 class testTail(TestCase):
@@ -9,6 +9,16 @@ class testTail(TestCase):
     def test_should_get_tail_to_iterable(self):
         result = tail(iter([1, 2, 3, 4]))
         self.assertEqual(result, [2, 3, 4])
+
+
+class TestHead(TestCase):
+    def test_get_head_list(self):
+        self.assertEqual(head([1, 2, 3, 4]), [1])
+        self.assertEqual(head([1, 2, 3, 4], 3), [1, 2, 3])
+
+    def test_get_head_string(self):
+        self.assertEqual(head("Olá BB!", 3), "Olá")
+        self.assertNotEqual(head("@python", 4), "@pyth")
 
 
 if __name__ == '__main__':
