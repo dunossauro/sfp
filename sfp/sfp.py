f6c1530e43bcff67b3375cccc1e6c242a3ea0a81
consoli <matheus.consoli7@gmail.com>
2018-03-24 16:29:57 -0300

    Compose function added.

--- sfp/sfp.py
+++ sfp/sfp.py
@@ -17,3 +17,10 @@ def pipe(*args):
 def _pipe(curr, prev):
     """Callback to `reduce` function."""
     return lambda x: prev(curr(x))
+
+
+def compose(*args):
+    """Composes all the given function in one."""
+    return reduce(lambda fun, tion: lambda arg: fun(tion(arg)),
+                  args,
+                  lambda arg: arg)
